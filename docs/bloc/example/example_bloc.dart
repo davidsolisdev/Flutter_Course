5d053195b4ea756f29f0613177848b9854b089c2
davidsolisdev <davidsolisdev@gmail.com>
2022-11-02 15:54:04 -0600

    v1

---
+++ docs/bloc/example/example_bloc.dart
@@ -0,0 +1,17 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/foundation.dart';
+
+part './example_state.dart';
+part './example_event.dart';
+
+class ExampleBloc extends Bloc<ExampleEvent, ExampleState> {
+  ExampleBloc() : super(const ExampleInitialState()) {
+    on<ChangeLogin>((event, emit) {
+      emit(ExampleModifyState(
+        event.lastState,
+        login: event.login,
+        token: event.token,
+      ));
+    });
+  }
+}
