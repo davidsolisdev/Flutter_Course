7c60b8499c1a85a41ed481a51715bd031ca446fe
davidsolisdev <davidsolisdev@gmail.com>
2022-11-14 09:00:01 -0600

    v1

--- docs/models/user.dart
+++ docs/models/user.dart
@@ -4,8 +4,14 @@ part 'user.g.dart';
 
 @JsonSerializable()
 class User {
-  @JsonKey(name: "")
+  @JsonKey(name: "nombre")
   String nombre;
 
   User({required this.nombre});
+
+  // FromJson
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+
+  // ToJson
+  Map<String, dynamic> toJson() => _$UserToJson(this);
 }
--- docs/widgets/material/scroll asincronos/list_view_builder.dart
+++ docs/widgets/material/scroll asincronos/list_view_builder.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_course/models/user.dart';
+
 class ListViewBuilder extends StatefulWidget {
   const ListViewBuilder({super.key});
 
@@ -8,11 +10,37 @@ class ListViewBuilder extends StatefulWidget {
 }
 
 class _ListViewBuilderState extends State<ListViewBuilder> {
-  final List<Map> lista = [];
+  // * lista de items
+  List<User> lista = <User>[];
+
+  // * controlador de scroll
+  final ScrollController _controller = ScrollController();
+
+  void scrollListener() {
+    if (_controller.position.pixels + 100 >=
+        _controller.position.maxScrollExtent) {
+      // * carga asincrona y llenado de lista de Users
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller.addListener(scrollListener);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    _controller.removeListener(scrollListener);
+  }
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
+      controller: _controller,
       itemCount: lista.length,
       itemBuilder: (context, index) {
         return Column(
